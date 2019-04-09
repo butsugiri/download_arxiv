e8848f17d4f7d4b51bbfb29bfe1d669c499b3c0c
Shun Kiyono <kiyono@ecei.tohoku.ac.jp>
2019-04-10 00:03:09 +0900

    output log

--- download_arxiv.py
+++ download_arxiv.py
@@ -4,24 +4,27 @@ import os
 import arxiv
 import click
 from dateutil.parser import parse
+from logzero import logger
 
 
 @click.command()
-@click.argument("url", type=str)
+@click.argument("urls", type=str, nargs=-1)
 @click.option(
     '-o',
     '--out',
     default='.',
     help='path to save pdf'
 )
-def main(url, out):
-    paper_id = os.path.basename(url)
-    paper = arxiv.query(id_list=[paper_id])[0]
+def main(urls, out):
+    for url in urls:
+        paper_id = os.path.basename(url)
+        paper = arxiv.query(id_list=[paper_id])[0]
 
-    def custom_slugify(obj):
-        author_last_name = obj['authors'][0].strip().split(' ')[-1]
-        year = parse(obj['published']).year
-        title = obj['title'].strip().replace('\n', '')
-        return '[{}+{}] {}'.format(author_last_name, year, title)
+        def custom_slugify(obj):
+            author_last_name = obj['authors'][0].strip().split(' ')[-1]
+            year = parse(obj['published']).year
+            title = obj['title'].strip().replace('\n', '')
+            logger.info('Download "{}" from "{}"'.format(title, obj['pdf_url']))
+            return '[{}+{}] {}'.format(author_last_name, year, title)
 
-    arxiv.download(paper, slugify=custom_slugify, dirpath=out)
+        arxiv.download(paper, slugify=custom_slugify, dirpath=out)
