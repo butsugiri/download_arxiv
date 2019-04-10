6be166be9e72e16eed6dbaebbb5ae8af563d4c4c
Shun Kiyono <kiyono@ecei.tohoku.ac.jp>
2019-04-10 22:48:13 +0900

    support pdf url

--- download_arxiv.py
+++ download_arxiv.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
+import time
+from urllib.request import urlretrieve
 
 import arxiv
 import click
@@ -7,8 +10,33 @@ from dateutil.parser import parse
 from logzero import logger
 
 
+def reporthook(count, block_size, total_size):
+    global start_time
+    if count == 0:
+        start_time = time.time()
+        return
+    duration = time.time() - start_time
+    progress_size = int(count * block_size)
+    speed = int(progress_size / (1024 * duration))
+    percent = int(count * block_size * 100 / total_size)
+    sys.stdout.write(
+        "\r{}%, {} KB, {} KB/s, {:.1f} seconds passed".format(min(percent, 100), progress_size / 1024, speed, duration))
+    sys.stdout.flush()
+
+
+def download(obj, slugify, dirpath='./'):
+    if not obj.get('pdf_url', ''):
+        print("Object has no PDF URL.")
+        return
+    if dirpath[-1] != '/':
+        dirpath += '/'
+    path = dirpath + slugify(obj) + '.pdf'
+    urlretrieve(obj['pdf_url'], path, reporthook=reporthook)
+    return path
+
+
 @click.command()
-@click.argument("urls", type=str, nargs=-1)
+@click.argument('urls', type=str, nargs=-1)
 @click.option(
     '-o',
     '--out',
@@ -18,7 +46,10 @@ from logzero import logger
 )
 def main(urls, out):
     for url in urls:
-        paper_id = os.path.basename(url)
+        if url.endswith('.pdf'):
+            paper_id = os.path.splitext(os.path.basename(url))[0]
+        else:
+            paper_id = os.path.basename(url)
         paper = arxiv.query(id_list=[paper_id])[0]
 
         def custom_slugify(obj):
@@ -28,4 +59,4 @@ def main(urls, out):
             logger.info('Download "{}" from "{}"'.format(title, obj['pdf_url']))
             return '[{}+{}] {}'.format(author_last_name, year, title)
 
-        arxiv.download(paper, slugify=custom_slugify, dirpath=out)
+        download(paper, slugify=custom_slugify, dirpath=out)
